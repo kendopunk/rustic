569bbf7fb158eb3ab31b04dcbf672eb9ee91cc2f
Mark Fehrenbacher <kendopunk@hotmail.com>
2023-01-11 17:20:37 -0500

    preparing some notes

--- linkedin/morse_code/src/main.rs
+++ linkedin/morse_code/src/main.rs
@@ -1,3 +1,40 @@
+/**
+ * Morse code
+ * Convert text (letters and numbers) to Morse Code
+ * (1) implement the MorseCode trait for String
+ * (2) Skip invalid input characters
+ * (3) Ignore case
+ */
+// trait MorseCode {
+//     fn to_morse_code(&self) -> Message;
+// }
+
+// type Message = Vec<Letter>
+// type Letter = Vec<Pulse>
+/**
+enum Pulse {
+    Short,
+    Long
+}
+*/
+
 fn main() {
     println!("Hello, world!");
 }
+
+/*Filename: src/lib.rs
+
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+ */
